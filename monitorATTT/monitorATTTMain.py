5c5966ba3e541af7c7dbaf345c55a0fc1d36cc04
keta124 <keta124@gmail.com>
2017-05-09 09:42:54 +0700

    Update monitorATTTMain.py

--- monitorATTT/monitorATTTMain.py
+++ monitorATTT/monitorATTTMain.py
@@ -8,6 +8,6 @@ import checkPortOpen
 if __name__ == '__main__':
     try:
         list_Login= checkSSHLogin.getListLogin()
-        checkPortOpen.getListPortOpen()
+        list_Port_Change= checkPortOpen.getListPortOpen()
     except:
         print "ERROR Except"
